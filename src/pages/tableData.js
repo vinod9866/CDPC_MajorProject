2c67e3eab9efd2d729640e487f54f4a07efaa367
r4m4k0735w4r4r40 <ramakoteswararao.mareedu9@gmail.com>
2022-04-11 14:24:45 +0530

    dashboard

--- src/pages/tableData.js
+++ src/pages/tableData.js
@@ -1,9 +1,10 @@
-import Card from "../ui/card";
 import Table from "./table";
 const DUMMY_DATA = [
   {
     id: "m1",
     title: "Google(Software Engineer)",
+    branches: ['CSE','ECE','CE'],
+    last_date: '11-04-2022',
     status: "Active",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
@@ -14,7 +15,57 @@ const DUMMY_DATA = [
   {
     id: "m2",
     title: "Napier Health Care(Analyst)",
-    status: "expired",
+    branches: ['CSE','ECE','CE','ME'],
+    last_date: '11-04-2022',
+    status: "Expired",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m3",
+    title: "Google(Software Engineer)",
+    branches: ['CSE','ECE','CE'],
+    last_date: '11-04-2022',
+    status: "Active",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m4",
+    title: "Napier Health Care(Analyst)",
+    branches: ['CSE','ECE','CE','ME'],
+    last_date: '11-04-2022',
+    status: "Expired",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m5",
+    title: "Google(Software Engineer)",
+    branches: ['CSE','ECE','CE'],
+    last_date: '11-04-2022',
+    status: "Active",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m6",
+    title: "Napier Health Care(Analyst)",
+    branches: ['CSE','ECE','CE','ME'],
+    last_date: '11-04-2022',
+    status: "Expired",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
@@ -23,21 +74,20 @@ const DUMMY_DATA = [
   },
 ];
 
-function TableData() {
+function TableData() { 
+
   return (
-    <Card>
-      {" "}
+    // <Card>
+    //   {" "}
       <div>
-        <h2>
-          <b>List Of Drives</b>
-        </h2>
-        <br></br>
         {DUMMY_DATA.map((meetup) => (
           <Table
             text="vinod"
             key={meetup.id}
             id={meetup.id}
             stat={meetup.status}
+            branches={meetup.branches}
+            last_date={meetup.last_date}
             title={meetup.title}
             image={meetup.image}
             address={meetup.address}
@@ -45,7 +95,6 @@ function TableData() {
           />
         ))}
       </div>
-    </Card>
   );
 }
 
