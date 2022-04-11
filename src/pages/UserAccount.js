c2521ad00fd97ab1908531b74c14bb23b0ce1a7e
vvsatyanarayana <veerasatyanarayana111@gmail.com>
2022-04-11 14:58:50 -0700

    Broadcast notifications & Profile page updated

--- src/pages/UserAccount.js
+++ src/pages/UserAccount.js
@@ -3,22 +3,29 @@ import img from "../auth/login.png";
 import { useState,useEffect } from 'react';
 import { Card } from 'react-bootstrap';
 import { Model } from './modal';
-import { getStudent } from '../apis';
+import { addStudentResume, getStudent } from '../apis';
 
 export const UserAccount =(props)=>{
 
     const [modal,setModal] = useState(false);
+    const [modal1,setModal1] = useState(false);
     const [acccoutData,setAccountData] = useState({})
     const showModel = ()=>{
         setModal(true);
     }
+    const showModel1 = ()=>{
+        setModal1(true);
+    }
     window.onClick = (event)=>{
-        console.log("working.......")
         setModal(false)
+        setModal1(false)
     }
 
     const handleModal = (bool)=>{
         setModal(bool);
+    }
+    const handleModal1 = (bool)=>{
+        setModal1(bool);
     }
 
     useEffect(()=>{
@@ -27,9 +34,26 @@ export const UserAccount =(props)=>{
         .then(data=>setAccountData(data));
     },[])
 
+    const updateProfile = (e) =>{
+        e.preventDefault()
+        addStudentResume()
+        .then(res=>res.json())
+        .then(result => result);
+    }
+
+    const updateResume = (e) =>{
+        e.preventDefault()
+        addStudentResume()
+        .then(res=>res.json())
+        .then(result => result);
+    }
+
+    const updateImage = () =>{
+
+    }
     return<Card className={classes.account}>
-    {modal?<Model parentCallback={handleModal} style={classes.modalClass} heading="Upload Resume to Apply Drive">
-        <form>
+        {modal1?<Model parentCallback={handleModal1} style={classes.modalClass} heading="Upload Resume to Apply Drive">
+        <form onSubmit={updateResume}>
         <div className={classes.field}>
             <input type="file" name='filename'></input>
         </div>
@@ -37,6 +61,66 @@ export const UserAccount =(props)=>{
             <input type="submit" value='Upload'></input>
         </div>
         </form>
+    </Model>:""}
+    {modal?<Model parentCallback={handleModal} style={classes.modalClass} heading="Upload Resume to Apply Drive">
+        <form className={classes.accountForm} onSubmit={updateProfile}>
+    <div className={classes.left}>
+        <div className={classes.field}>
+            <label>Name</label><br/>
+            <input type="text" value={acccoutData.username}></input>
+        </div>
+        <div className={classes.field}>
+            <label>ID</label><br/>
+            <input type="text" value={acccoutData.userId}></input>
+        </div>
+        <div className={classes.field}>
+            <label>Email</label><br/>
+            <input type="text" value={acccoutData.useremail}></input>
+        </div>
+        <div className={classes.field}>
+            <label>DOB</label><br/>
+            <input type="text" value={acccoutData.dob}></input>
+        </div>
+        <div className={classes.field}>
+            <label>YEAR</label><br/>
+            <input type="text" value={acccoutData.year}></input>
+        </div>
+        <div className={classes.field}>
+            <label>Year Of Pass</label><br/>
+            <input type="text" value={acccoutData.username}></input>
+        </div>
+    </div>
+    <div className={classes.right}>
+        <div className={classes.field}>
+            <label>YEAR</label><br/>
+            <input type="text" value={acccoutData.username}></input>
+        </div>
+        <div className={classes.field}>
+            <label>BRANCH</label><br/>
+            <input type="text"  value={acccoutData.branch}></input>
+        </div>
+        <div className={classes.field}>
+            <label>PUC_CGPA</label><br/>
+            <input type="text" value={acccoutData.pucCgpa}></input>
+        </div>
+        <div className={classes.field}>
+            <label>ENG_CGPA</label><br/>
+            <input type="text" value={acccoutData.engCgpa}></input>
+        </div>
+
+        <div className={classes.field}>
+            <label>Passed</label><br/>
+            <input type="text" value={acccoutData.isPass}></input>
+        </div>
+        <div className={classes.field}>
+            <label>REM_Count</label><br/>
+            <input type="text" value={acccoutData.count}></input>
+        </div>
+        <div className={classes.field}>
+            <input type="submit" value='Upload'></input>
+        </div>
+    </div>
+</form>
     </Model>:""}
 <div className={classes.profile}>
     <img src={img} alt='not working'/>
@@ -99,7 +183,8 @@ export const UserAccount =(props)=>{
     </div>
 </form>
 <div className={classes.space}>
-    <button className={classes.resume} onClick={showModel}>ADD or Update Resume</button>
+<button className={classes.resume} onClick={showModel}>Update Profile</button>
+    <button className={classes.resume} onClick={showModel1}>Update Resume</button>
     
 </div>
 </Card>
